e1a356866372133e61864f6d798b87745378a7af
JiangShuuu <globelex65@gmail.com>
2022-04-24 15:45:07 +0800

    upd: show Data

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const { engine } = require("express-handlebars");
 const port = 3000;
 const app = express();
-const restaurantList = require("./restaurant.json");
+// const restaurantList = require("./restaurant.json");
+const Restaurant = require ('./models/restaurant')
 
 require('dotenv').config()
 const mongoose = require('mongoose')
@@ -24,28 +25,49 @@ app.use(express.static("public"));
 
 // index頁面 餐廳data
 app.get("/", (req, res) => {
-  res.render("index", { restaurants: restaurantList.results });
+  Restaurant.find()
+    .lean()
+    .then( resData => res.render('index', { resData }))
+    .catch(err => console.log(err))
 });
 
 // show頁面 餐廳詳細資料
-app.get("/restaurants/:restaurant_id", (req, res) => {
-  const restaurant = restaurantList.results.find(
-    (res) => res.id.toString() === req.params.restaurant_id
-  );
-  res.render("show", { restaurantInfo: restaurant });
+app.get("/restaurants/:restaurantId", (req, res) => {
+  const { restaurantId } = req.params
+  Restaurant.findById(restaurantId)
+    .lean()
+    .then(resInfo => res.render("show", { resInfo }))
+    .catch(err => console.log(err))
 });
+
+
 
 //search功能
 app.get("/search", (req, res) => {
   // console.log("req.query", req.query);
-  const keyword = req.query.keyword;
-  const restaurantSearch = restaurantList.results.filter((restau) => {
-    return (
-      restau.category.toLowerCase().includes(keyword.toLowerCase()) ||
-      restau.name.toLowerCase().includes(keyword.toLowerCase())
-    );
-  });
-  res.render("index", { restaurants: restaurantSearch });
+  if (!req.query.keywords) {
+    res.redirect("/")
+  }
+
+  const keywords = req.query.keywords
+  const keyword = req.query.keywords.trim().toLowerCase()
+  
+  Restaurant.find({})
+    .lean()
+    .then(resData => {
+      const filterResData = resData.filter(
+        data => data.name.toLowerCase().includes(keyword) || data.category.includes(keyword)
+      )
+      res.render("index", {resData: filterResData, keywords})
+    })
+
+  // const restaurantSearch = restaurantList.results.filter((restau) => {
+  //   return (
+  //     restau.category.toLowerCase().includes(keyword.toLowerCase()) ||
+  //     restau.name.toLowerCase().includes(keyword.toLowerCase())
+  //   );
+  // });
+  // res.render("index", { restaurants: restaurantSearch });
 });
 
 app.listen(port, () => {
