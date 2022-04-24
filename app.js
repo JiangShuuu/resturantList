7ab5dc64ff0760698f025b0edc3fcb14ed96b955
JiangShuuu <globelex65@gmail.com>
2022-04-24 16:00:07 +0800

    add CRUD

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3000;
 const app = express();
 // const restaurantList = require("./restaurant.json");
 const Restaurant = require ('./models/restaurant')
+const methodOverride = require("method-override")
 
 require('dotenv').config()
 const mongoose = require('mongoose')
@@ -22,6 +23,8 @@ app.engine(
 );
 app.set("view engine", "handlebars");
 app.use(express.static("public"));
+app.use(methodOverride("_method"))
+app.use(express.urlencoded({ extended: true }))
 
 // index頁面 餐廳data
 app.get("/", (req, res) => {
@@ -60,15 +63,41 @@ app.get("/search", (req, res) => {
       )
       res.render("index", {resData: filterResData, keywords})
     })
-
-  // const restaurantSearch = restaurantList.results.filter((restau) => {
-  //   return (
-  //     restau.category.toLowerCase().includes(keyword.toLowerCase()) ||
-  //     restau.name.toLowerCase().includes(keyword.toLowerCase())
-  //   );
-  // });
-  // res.render("index", { restaurants: restaurantSearch });
 });
+
+// 新增餐廳
+app.post("/restaurants", (req, res) => {
+  Restaurant.create(req.body)
+    .then(() => res.redirect("/"))
+    .catch(err => console.log(err))
+})
+
+// 編輯餐廳頁面
+app.get("/restaurants/:restaurantId/edit", (req, res) => {
+  const { restaurantId } = req.params
+  Restaurant.findById(restaurantId)
+    .lean()
+    .then(restaurantData => res.render("edit", { restaurantData }))
+    .catch(err => console.log(err))
+})
+
+// 更新餐廳
+app.put("/restaurants/:restaurantId", (req, res) => {
+  console.log(1)
+  const { restaurantId } = req.params
+  Restaurant.findByIdAndUpdate(restaurantId, req.body)
+    //可依照專案發展方向自定編輯後的動作，這邊是導向到瀏覽特定餐廳頁面
+    .then(() => res.redirect(`/restaurants/${restaurantId}`))
+    .catch(err => console.log(err))
+})
+
+// 刪除餐廳
+app.delete("/restaurants/:restaurantId", (req, res) => {
+  const { restaurantId } = req.params
+  Restaurant.findByIdAndDelete(restaurantId)
+    .then(() => res.redirect("/"))
+    .catch(err => console.log(err))
+})
 
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`);
