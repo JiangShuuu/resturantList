94b8941e762a1f7ceff0e65eda1611e1cdf720ed
JiangShuuu <globelex65@gmail.com>
2022-05-20 11:12:07 +0800

    add restaurant router

--- routers/index.js
+++ routers/index.js
@@ -3,8 +3,11 @@ const router = express.Router()
 
 // 引入路由模組
 const home = require('./modules/home')
+const restaurants = require('./modules/restaurants')
 
 // 使用模組
 router.use('/', home)
+router.use('/restaurants', restaurants)
 
+// 輸出模組
 module.exports = router
--- routers/modules/home.js
+++ routers/modules/home.js
@@ -10,4 +10,23 @@ router.get("/", (req, res) => {
     .catch(err => console.log(err))
 });
 
+router.get("/search", (req, res) => {
+  // console.log("req.query", req.query);
+  if (!req.query.keywords) {
+    res.redirect("/")
+  }
+
+  const keywords = req.query.keywords
+  const keyword = req.query.keywords.trim().toLowerCase()
+  
+  Restaurant.find({})
+    .lean()
+    .then(resData => {
+      const filterResData = resData.filter(
+        data => data.name.toLowerCase().includes(keyword) || data.category.includes(keyword)
+      )
+      res.render("index", {resData: filterResData, keywords})
+    })
+});;
+
 module.exports = router;
