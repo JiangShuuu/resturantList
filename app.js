cb6451a9664e5b397cb51621c3a6c626255a87fc
JiangShuuu <globelex65@gmail.com>
2022-05-20 11:35:36 +0800

    upd seeds mongoose

--- app.js
+++ app.js
@@ -2,22 +2,12 @@ const express = require("express");
 const { engine } = require("express-handlebars");
 const port = 3000;
 const app = express();
-// const restaurantList = require("./restaurant.json");
-const Restaurant = require ('./models/restaurant')
 const methodOverride = require("method-override")
 
 const routes = require('./routers')
 
-require('dotenv').config()
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')
+
 
 app.engine(
   "handlebars",
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,13 +1,8 @@
-const mongoose = require('mongoose')
-const Restaurant = require('../restaurant')
 const restaurantList = require("../../restaurant.json").results
+const Restaurant = require('../restaurant')
 
-require('dotenv').config()
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
+const db = require('../../config/mongoose')
+
 db.once('open', () => {
   console.log("running restaurantSeeder script...")
 
