7a74d360abe3545decd169babd712887ceb0e723
JiangShuuu <globelex65@gmail.com>
2022-05-21 11:02:56 +0800

    add user modules

--- routers/index.js
+++ routers/index.js
@@ -4,10 +4,12 @@ const router = express.Router()
 // 引入路由模組
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const users = require('./modules/users')
 
 // 使用模組
 router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/users', users)
 
 // 輸出模組
 module.exports = router
